0b4561719b9aebe35a65378e113775d707701c2a
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2022-03-01 21:46:51 -0600

    Dark mode config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import * as React from "react"
-import { BrowserRouter } from "react-router-dom";
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './styles/theme'
 import Nav from "../components/Nav";
 
 const IndexPage = () => {
+  const [theme, setTheme] = useState("light");
+  const isDarkTheme = theme === "dark";
+  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
+  
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <>
+        <GlobalStyles />
+        <h1>Hey there!</h1>
+        <button onClick={toggleTheme}>
+            {isDarkTheme ?
+              <span aria-label="Light mode" role="img">🌞</span> :
+              <span aria-label="Dark mode" role="img">🌜</span>}
+        </button>
         <Nav />
-    </BrowserRouter>
-   
+      </>
+   </ThemeProvider>
   )
 } 
 
