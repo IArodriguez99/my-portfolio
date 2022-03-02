e88f96cf715695067e6b1ead30ef3b65176bd391
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2022-03-02 13:41:12 -0600

    Moving dark mode toggle to nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,13 @@
 import React, { useEffect } from "react";
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from '../pages/styles/theme'
 
 export default function Nav() {
 
+const [theme, setTheme] = useState("light");
+  const isDarkTheme = theme === "dark";
+  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
     useEffect(() => {
         let url = window.location.href.split("/");
         let target = url[url.length - 1].toLowerCase();
@@ -10,42 +16,50 @@ export default function Nav() {
       }, []);
 
   return (
-    <nav>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <nav>
+        <GlobalStyles />
+          <button onClick={toggleTheme}>
+              {isDarkTheme ?
+                <span aria-label="Light mode" role="img">🌞</span> :
+                <span aria-label="Dark mode" role="img">🌜</span>}
+          </button>
+          <a
+          href="/"
+          onClick={e => {
+            let about = document.getElementById("about");
+            e.preventDefault();
+            about && about.scrollIntoView({ behavior: "smooth", block: "start" });
+            window.history.pushState("about", "about", "/about");
+          }}
+        >
+          About Me
+        </a>
         <a
-        href="/"
-        onClick={e => {
-          let about = document.getElementById("about");
-          e.preventDefault();
-          about && about.scrollIntoView({ behavior: "smooth", block: "start" });
-          window.history.pushState("about", "about", "/about");
-        }}
-      >
-        About Me
-      </a>
-      <a
-        href="/"
-        onClick={e => {
-          let projects = document.getElementById("projects");
-          e.preventDefault();
-          projects && projects.scrollIntoView({ behavior: "smooth", block: "start" });
-          window.history.pushState("projects", "projects", "/projects");
-        }}
-      >
-        Projects
-      </a>
-      <a
-        href="/"
-        onClick={e => {
-          let contact = document.getElementById("contact");
-          e.preventDefault();
-          contact &&
-            contact.scrollIntoView({ behavior: "smooth", block: "start" });
-          window.history.pushState("contact", "contact", "/contact");
-        }}
-      >
-        Contact
-      </a>
-    </nav>
+          href="/"
+          onClick={e => {
+            let projects = document.getElementById("projects");
+            e.preventDefault();
+            projects && projects.scrollIntoView({ behavior: "smooth", block: "start" });
+            window.history.pushState("projects", "projects", "/projects");
+          }}
+        >
+          Projects
+        </a>
+        <a
+          href="/"
+          onClick={e => {
+            let contact = document.getElementById("contact");
+            e.preventDefault();
+            contact &&
+              contact.scrollIntoView({ behavior: "smooth", block: "start" });
+            window.history.pushState("contact", "contact", "/contact");
+          }}
+        >
+          Contact
+        </a>
+      </nav>
+    </ThemeProvider>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,11 @@
 import * as React from "react"
-import { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme, GlobalStyles } from './styles/theme'
 import Nav from "../components/Nav";
 
 const IndexPage = () => {
-  const [theme, setTheme] = useState("light");
-  const isDarkTheme = theme === "dark";
-  const toggleTheme = () => setTheme(isDarkTheme ? "light" : "dark");
-  
   return (
-    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <>
-        <GlobalStyles />
-        <h1>Hey there!</h1>
-        <button onClick={toggleTheme}>
-            {isDarkTheme ?
-              <span aria-label="Light mode" role="img">🌞</span> :
-              <span aria-label="Dark mode" role="img">🌜</span>}
-        </button>
         <Nav />
       </>
-   </ThemeProvider>
   )
 } 
 
